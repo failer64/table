264b40b37548a649b8cb212d18eb97656c52bf32
failer64 <failer64@rambler.ru>
2024-01-25 00:26:47 +0300

    dfgdfg

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: 'table-rep.vercel.app',
+	//base: 'vercel.app',
 	//table-rep.vercel.app
 })
